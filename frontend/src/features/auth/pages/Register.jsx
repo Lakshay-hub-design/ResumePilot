57a959f952bafd825f9ac0a162be2b483b624756
Lakshay Sharma <lakshay0328@gmail.com>
2026-03-03 12:58:23 +0530

    login and register ui build and service layer implemented

--- frontend/src/features/auth/pages/Register.jsx
+++ frontend/src/features/auth/pages/Register.jsx
@@ -1,10 +1,39 @@
 import React from 'react'
+import { Link, useNavigate } from 'react-router'
 
 const Register = () => {
+
+  const navigate = useNavigate()
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div>
-      
-    </div>
+    <main>
+      <div className="form-container">
+          <h1>Register</h1>
+
+          <form onSubmit={handleSubmit}>
+            <div className="input-group">
+              <label htmlFor="username">Username</label>
+              <input type="text" id="username" name="username" placeholder='Enter username' required />
+            </div>
+            <div className="input-group">
+              <label htmlFor="email">Email</label>
+              <input type="email" id="email" name="email" placeholder='Enter email address' required />
+            </div>
+            <div className="input-group">
+              <label htmlFor="password">Password</label>
+              <input type="password" id="password" name="password" placeholder='Enter password' required />
+            </div>
+
+            <button className='button primary-button'>Register</button>
+          </form>
+
+          <p>Already have an account? <Link to={'/login'}>Login</Link></p>
+      </div>
+    </main>
   )
 }
 
